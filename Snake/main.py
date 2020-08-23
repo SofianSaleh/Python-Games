3968b3aff7f64a3a3d0a78a65a11ec34fe34a212
SofianSaleh <sofiane.saleh11@gmail.com>
2020-08-23 17:12:58 +0200

    :fire: doing the structures

--- Snake/main.py
+++ Snake/main.py
@@ -9,13 +9,19 @@ class Snake():
         self.positions = [((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))]
         self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
         self.color = (17, 24, 47)
-        # Special thanks to YouTubers Mini - Cafetos and Knivens Beast for raising this issue!
-        # Code adjustment courtesy of YouTuber Elija de Hoog
         self.score = 0
+
+    def get_head_position(self):
+        return self.positions[0]
+
+    def turn(self, point):
+        if self.length > 1 and (point[0]*-1, point[1]*-1) == self.direction:
+            return
+        else:
+            self.direction = point
 
 
 # Constants
-
 SCREEN_HEIGHT = 480
 SCREEN_WIDTH = 480
 
