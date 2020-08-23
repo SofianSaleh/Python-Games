49c106cccf63ffe2d39169b477f716c25f0f7962
SofianSaleh <sofiane.saleh11@gmail.com>
2020-08-23 17:13:12 +0200

    :fire: doing the structures

--- Snake/main.py
+++ Snake/main.py
@@ -20,6 +20,18 @@ class Snake():
         else:
             self.direction = point
 
+    def move(self):
+        cur = self.get_head_position()
+        x, y = self.direction
+        new = (((cur[0]+(x*GRIDSIZE)) % SCREEN_WIDTH),
+               (cur[1]+(y*GRIDSIZE)) % SCREEN_HEIGHT)
+        if len(self.positions) > 2 and new in self.positions[2:]:
+            self.reset()
+        else:
+            self.positions.insert(0, new)
+            if len(self.positions) > self.length:
+                self.positions.pop()
+
 
 # Constants
 SCREEN_HEIGHT = 480
