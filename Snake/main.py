88ed8d58e6fefd156859d24a2b4754de87c8ed21
SofianSaleh <sofiane.saleh11@gmail.com>
2020-08-23 17:14:45 +0200

    :fire: doing the food class

--- Snake/main.py
+++ Snake/main.py
@@ -76,6 +76,18 @@ class Food():
         pygame.draw.rect(surface, self.color, r)
         pygame.draw.rect(surface, (93, 216, 218), r, 1)
 
+    def draw_grid(surface):
+    for y in range(0, int(GRID_HEIGHT)):
+        for x in range(0, int(GRID_WIDTH)):
+            if (x + y) % 2 == 0:
+                r = pygame.Rect((x * GRIDSIZE, y * GRIDSIZE),
+                                (GRIDSIZE, GRIDSIZE))
+                pygame.draw.rect(surface, (93, 216, 228), r)
+            else:
+                rr = pygame.Rect((x * GRIDSIZE, y * GRIDSIZE),
+                                 (GRIDSIZE, GRIDSIZE))
+                pygame.draw.rect(surface, (84, 194, 205), rr)
+
 
 
 # Constants
