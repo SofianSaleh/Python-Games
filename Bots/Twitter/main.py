713234d22e7bb7c4985ab2a8d9f8be991097d724
SofianSaleh <sofiane.saleh11@gmail.com>
2020-08-26 13:49:08 +0200

    :fire:

--- Bots/Twitter/main.py
+++ Bots/Twitter/main.py
@@ -1,5 +1,25 @@
 import tweepy
 import time
+import os
+# print(os.environ.get('consumer_key'))
 
 
-auth = tweepy.OAuthHandler()
+auth = tweepy.OAuthHandler("", "")
+auth.set_access_token("", "")
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+search = "Python"
+nrTweets = 100
+
+for tweet in tweepy.cursor(api.search, search).items(nrTweets):
+    try:
+        print('tweet liked')
+        tweet.favorite()
+        time.sleep(5)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopAsyncIteration:
+        break
